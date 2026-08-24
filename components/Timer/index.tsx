421e4f7b43e1201e815a9ceff6ab92d9c84658d6
Lesstread666 <vlamaks1604@gmail.com>
2026-08-24 16:41:34 +0200

    add comments

--- components/Timer/index.tsx
+++ components/Timer/index.tsx
@@ -5,22 +5,29 @@ type TimerProps = {
 }
 
 const Timer = ({ startTime }: TimerProps) => {
+    //useState to control if timer started
     const [timerStarted, setTimerStarted] = useState(false)
+
+    //useState to store the current number of seconds left 
+    // value comes from startTime prop
     const [remainingSeconds, setRemainingSeconds] = useState(startTime)
 
     useEffect(() => {
+        //don't create interval if timer not started or seconds has reached 0
         if (!timerStarted || remainingSeconds === 0) return
 
+        //runs interval each 1 sec and decrease the time by 1
         const interval = setInterval(() => {
             setRemainingSeconds(
                 (previousSeconds) => previousSeconds - 1
             )
         }, 1000)
 
+        //cleans up current interval before useEffect runs again
         return () => clearInterval(interval)
     }, [timerStarted, remainingSeconds])
 
-
+    //reset the timer to original number and stops the timer
     const resetTimer = () => {
         setRemainingSeconds(startTime)
         setTimerStarted(false)
