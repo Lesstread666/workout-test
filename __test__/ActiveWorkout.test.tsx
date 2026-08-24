630079a9bce66f94ca9384753af0aee1165e4ecb
Lesstread666 <vlamaks1604@gmail.com>
2026-08-24 17:35:03 +0200

    ActiveWorkout prop created using test

--- __test__/ActiveWorkout.test.tsx
+++ __test__/ActiveWorkout.test.tsx
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react'
-import ActiveWorkout from '.'
+import ActiveWorkout from '@/components/ActiveWorkout'
 
 describe("The ActiveWorkout works ok", () => {
 
